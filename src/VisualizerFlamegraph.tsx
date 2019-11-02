bef80b3838b52443f6183ecc6d3a67aa47ab2a16
Felix Geisendörfer <felix@debuggable.com>
2019-11-02 13:36:10 +0100

    Get initial flamegraph stuff to work

--- src/VisualizerFlamegraph.tsx
+++ src/VisualizerFlamegraph.tsx
@@ -1,22 +1,64 @@
 import React from 'react';
+import {fromPlan} from './lib/Convert';
+import {flamegraph} from 'd3-flame-graph';
+import {Node as FlameNode} from './lib/FlameJSON';
+import * as d3 from 'd3';
+import 'd3-flame-graph/src/flamegraph.css'
 
 interface Props {
     planText: string,
 }
 
-export default function VisualizerFlamegraph(p: Props) {
-    let parsed:{};
-    try {
-        parsed = JSON.parse(p.planText)
-    } catch (e) {
-        parsed = {}
+export interface FoldedJSON {
+    name: string,
+    value: number,
+    children?: FoldedJSON[],
+}
+
+function flameJSON(n: FlameNode): FoldedJSON  {
+    if (!('Inclusive Time' in n)) {
+        throw new Error('bad n');
     }
+    return {
+        name: n.Label,
+        value: n["Inclusive Time"],
+        children: (n.Children || []).map(flameJSON),
+    }
+}
+
+export default function VisualizerFlamegraph(p: Props) {
+    const flameRef = React.useRef<HTMLDivElement>(null);
+
+    let fudged: FoldedJSON = {} as FoldedJSON;
+    try {
+        let data = JSON.parse(p.planText);
+        let node = fromPlan(data);
+        fudged = flameJSON(node);
+    } catch (e) {
+        // TODO handle
+    }
+
+    React.useEffect(() => {
+        if (!flameRef.current) {
+            return;
+        }
+
+        const fg = flamegraph()
+            .width(flameRef.current.offsetWidth)
+            .sort(true);
+
+        d3.select(flameRef.current)
+            .datum(fudged)
+            .call(fg);
+        return () => {
+            fg.destroy();
+        }
+    })
+
 
     return <section className="section">
         <div className="container">
-            <p style={{whiteSpace: 'pre-wrap'}}>
-            {JSON.stringify(parsed, null, 2)}
-            </p>
+            <div ref={flameRef} className="flamegraph" />
         </div>
     </section>;
 };
