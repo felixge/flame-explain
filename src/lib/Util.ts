ae72ef595ba23e924b05bba3ff2488c732f07d47
Felix Geisendörfer <felix@debuggable.com>
2019-11-15 13:14:09 +0100

    util: add an assert function

--- src/lib/Util.ts
+++ src/lib/Util.ts
@@ -1,1 +1,7 @@
 export type OptionalEmbed<T, Fragment> = T | (T & Fragment);
+
+export function assert(condition: any, msg?: string): asserts condition {
+    if (!condition) {
+        throw new Error(msg);
+    }
+}
