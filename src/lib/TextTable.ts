cf836f35be6503fcde7561f9a182999cd321ccec
Felix Geisendörfer <felix@debuggable.com>
2019-12-19 15:02:55 +0100

    initial TextTable utility

---
+++ src/lib/TextTable.ts
@@ -0,0 +1,72 @@
+import {Node as FNode} from './FlameExplain';
+import {formatDuration} from './Util';
+// @ts-ignore no type definitions
+import AsciiTable from 'ascii-table';
+
+type Column =
+  '#' |
+  'Actual Loops' |
+  'Actual Rows' |
+  'Actual Total Time' |
+  'Label' |
+  'Node Type' |
+  'Self Time' |
+  'Total Time' |
+  'Virtual';
+
+export function textTable(n: FNode, cols: Column[]): string {
+  let table = new AsciiTable();
+  table.setHeading(...cols);
+
+  let rowNum = 1;
+  const visit = (n: FNode, depth = 0) => {
+    if (!n.Root) {
+      const colVals = cols.map(c => extractColumn(n, c, rowNum, depth));
+      table.addRow(...colVals);
+      rowNum++;
+      depth++;
+    }
+
+    (n.Children || []).forEach(child => visit(child, depth));
+  }
+  visit(n);
+
+  return table.toString();
+}
+
+export function extractColumn(n: FNode, c: Column, rowNum: number, depth: number): any {
+  switch (c) {
+    case '#':
+      return rowNum;
+    case 'Actual Loops':
+      return ('Actual Loops' in n.Source)
+        ? n.Source["Actual Loops"]
+        : '';
+    case 'Actual Rows':
+      return ('Actual Rows' in n.Source)
+        ? n.Source["Actual Rows"]
+        : '';
+    case 'Actual Total Time':
+      return ('Actual Total Time' in n.Source)
+        ? formatDuration(n.Source["Actual Total Time"])
+        : '';
+    case 'Label':
+      return '  '.repeat(depth) + n.Label;
+    case 'Node Type':
+      return ('Node Type' in n.Source)
+        ? n.Source["Node Type"]
+        : '';
+    case 'Self Time':
+      return ('Self Time' in n)
+        ? formatDuration(n["Self Time"])
+        : '';
+    case 'Total Time':
+      return ('Total Time' in n)
+        ? formatDuration(n["Total Time"])
+        : '';
+    case 'Virtual':
+      return (n.Virtual)
+        ? 'x'
+        : '';
+  }
+}
