74dc0d6481332d90dfe89c4d26445dd36f9483af
Felix Geisendörfer <felix@debuggable.com>
2020-02-07 19:01:30 +0100

    add some untracted files

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+// Make ts shut up about import issues in Highlight.tsx.
+// see https://github.com/microsoft/TypeScript/issues/15031
+declare module 'prismjs/plugins/custom-class/prism-custom-class';
+declare module 'prismjs/components/prism-sql';
+declare module 'prismjs/components/prism-json';
+declare module 'prismjs/components/prism-core';
+declare module 'prismjs/components/prism-clike';
+declare module 'prismjs/components/prism-javascript';
