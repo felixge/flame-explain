5993effdd5abb81ddaeea6658532218be32a884b
Felix Geisendörfer <felix@debuggable.com>
2020-05-04 11:37:28 +0200

    Include build version in footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 export default function Footer() {
+  const version = process.env.REACT_APP_VERSION || 'N/A';
   return (
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <strong>FlameExplain<span role="img" aria-label="trademark">™️</span></strong> by <a href="https://felixge.de/">Felix Geisendörfer</a> at fsync GmbH.
+          <strong>FlameExplain<span role="img" aria-label="trademark">™️</span></strong>
+          &nbsp;version <a href="https://github.com/felixge/flame-explain/releases">{version}</a>
+          &nbsp;by <a href="https://felixge.de/">Felix Geisendörfer</a> at fsync GmbH.
           All rights reserved.
         </p>
       </div>
