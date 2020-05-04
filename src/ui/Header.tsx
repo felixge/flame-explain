773d18562b74ecbdd1d6b29a0f6b3fda2976dd3d
Felix Geisendörfer <felix@debuggable.com>
2020-05-04 15:26:45 +0200

    Remove hero banner
    Takes up too much space

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,41 +1,26 @@
 import React from 'react';
-import {Link, NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a href="/" className="navbar-item"><span role="img" aria-label="flame">🔥</span>&nbsp;FlameExplain</a>
-        </div>
+    <nav className="navbar is-info" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a href="/" className="navbar-item"><span role="img" aria-label="flame">🔥</span>&nbsp;FlameExplain</a>
+      </div>
 
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <NavLink
-              activeClassName="is-active"
-              to="/visualize/input"
-              className="navbar-item"
-              isActive={(_, {pathname}) => pathname.startsWith('/visualize')}
-            >Visualize</NavLink>
-            <NavLink activeClassName="is-active" to="/docs" className="navbar-item">Documentation</NavLink>
-            <NavLink activeClassName="is-active" to="/about" className="navbar-item">About</NavLink>
-            <NavLink activeClassName="is-active" to="/credits" className="navbar-item">Credits</NavLink>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-            </div>
-          </div>
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <NavLink
+            activeClassName="is-active"
+            to="/visualize/input"
+            className="navbar-item"
+            isActive={(_, {pathname}) => pathname.startsWith('/visualize')}
+          >Visualize</NavLink>
+          <NavLink activeClassName="is-active" to="/docs" className="navbar-item">Documentation</NavLink>
+          <NavLink activeClassName="is-active" to="/about" className="navbar-item">About</NavLink>
+          <NavLink activeClassName="is-active" to="/credits" className="navbar-item">Credits</NavLink>
         </div>
-      </nav>
-      <section className="hero is-info is-bold">
-        <div className="hero-body">
-          <h1 className="title">FlameExplain</h1>
-          <h2 className="subtitle">
-            The next generation PostgreSQL EXPLAIN visualizer that <Link to="/about" className="has-text-warning">gets the numbers right</Link>.
-            </h2>
-        </div>
-      </section>
-    </div>
+      </div>
+    </nav>
   );
 };
